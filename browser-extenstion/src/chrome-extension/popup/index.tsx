eda8120338ef68fdce9ce78ea809e52b52810fb3
equationalapplications <info@equationalapplications.com>
2025-02-08 20:07:50 -0500

    add session listener

--- browser-extenstion/src/chrome-extension/popup/index.tsx
+++ browser-extenstion/src/chrome-extension/popup/index.tsx
@@ -1,10 +1,29 @@
+import { useEffect, useState } from "react";
 import "../global.css";
+import { supabase } from "../lib/supabaseClient";
+import { Session } from "@supabase/supabase-js";
 
 export const Popup = () => {
+  const [session, setSession] = useState<Session | null>(null);
+  useEffect(() => {
+    supabase.auth.onAuthStateChange((event, session) => {
+      setSession(session);
+      console.log("Auth state changed:", event, session);
+    })
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
+  }, [supabase]);
+
+  useEffect(() => {
+    if (!session) {
+      chrome.tabs.create({ url: process.env.VITE_WEB_AUTH_URL });
+    }
+  }, [session]);
 
   return (
     <div className="text-5xl p-10 font-extrabold">
-      <p>Equational Agent</p>
+      {session ? <p>Equational Agent</p> : <p>Please sign in</p>}
     </div>
   );
 };
